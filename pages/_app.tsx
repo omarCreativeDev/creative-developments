5910a0cc76748eef0a7d9ee3faaa20f3fc51da4d
Omar Mirza <omar.creative.dev@gmail.com>
2022-10-11 22:15:13 +0100

    added prime react, normalize css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from 'next/app';
 import { Layout } from '../components/Layout/Layout';
 
+import '../styles/styles.scss';
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <Layout>
