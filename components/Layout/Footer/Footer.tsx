ebdd1e635140ac2dd80298fa5581ce7b9371d01d
Omar Mirza <omar.creative.dev@gmail.com>
2022-10-31 21:14:11 +0000

    wip footer

--- components/Layout/Footer/Footer.tsx
+++ components/Layout/Footer/Footer.tsx
@@ -1,3 +1,25 @@
+import { faArrowUp, faCode } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 export const Footer = () => {
-  return <p>Footer</p>;
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  return (
+    <footer>
+      <div className="container m-auto p-4">
+        <div className="grid grid-flow-col auto-cols-auto justify-between">
+          <span className="hidden sm:block">
+            <FontAwesomeIcon className="pr-3" icon={faCode} />
+            Developing &amp; freelancing
+          </span>
+          <span className="cursor-pointer" onClick={scrollToTop}>
+            <span className="text-tertiary">Back to Top</span>
+            <FontAwesomeIcon className="pl-3" icon={faArrowUp} />
+          </span>
+        </div>
+      </div>
+    </footer>
+  );
 };
