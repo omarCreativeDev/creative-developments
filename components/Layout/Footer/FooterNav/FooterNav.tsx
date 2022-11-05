49350a46ffa76170ca834e2cf230e3421e3439fb
Omar Mirza <omar.creative.dev@gmail.com>
2022-11-05 20:41:31 +0000

    wip updates to footer nav for grid layout

--- components/Layout/Footer/FooterNav/FooterNav.tsx
+++ components/Layout/Footer/FooterNav/FooterNav.tsx
@@ -42,7 +42,25 @@ export const FooterNav = () => {
   };
   return (
     <div className={`${navBg} py-16`}>
-      <div className="container m-auto px-4 text-white">
+      <div className="container m-auto px-4 text-white grid gap-x-5 gap-y-10 grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
+        <dl>
+          <dt className={`pb-7 uppercase text-lg ${description}`}>{browserIconList.description}</dt>
+          {browserIconList.items.map((i) => (
+            <dd key={i.class} className="inline-grid pr-4">
+              <FontAwesomeIcon className={`${i.class} text-5xl`} icon={i.icon}></FontAwesomeIcon>
+            </dd>
+          ))}
+        </dl>
+
+        <dl>
+          <dt className={`pb-7 uppercase text-lg ${description}`}>{browserIconList.description}</dt>
+          {browserIconList.items.map((i) => (
+            <dd key={i.class} className="inline-grid pr-4">
+              <FontAwesomeIcon className={`${i.class} text-5xl`} icon={i.icon}></FontAwesomeIcon>
+            </dd>
+          ))}
+        </dl>
+
         <dl>
           <dt className={`pb-7 uppercase text-lg ${description}`}>{browserIconList.description}</dt>
           {browserIconList.items.map((i) => (
