4f80e99d40f5319a612ed391bd94382ef5e2026e
Omar Mirza <omar.creative.dev@gmail.com>
2022-11-06 16:26:38 +0000

    added wip intro component for home page

---
+++ components/Index/Intro/Intro.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+
+export const Intro = () => {
+  return (
+    <div className="container">
+      <h1 className="text-center">
+        HI! <span className="text-tertiary">I&apos;M OMAR</span>, & I&apos;M A FREELANCE
+        <span className="text-tertiary"> FRONT END</span> WEB
+        <span className="text-tertiary"> DEVELOPER</span>.
+      </h1>
+      <p className="text-center">
+        I have 14 years solid industry experience and I&apos;m available for&nbsp;
+        <Link href="/contact-me">
+          <a className="text-tertiary underline">hire</a>
+        </Link>
+        &nbsp;as a full time front end developer on a contract basis. Welcome to my online&nbsp;
+        <Link href="/contact-me">
+          <a className="text-tertiary underline">portfolio</a>
+        </Link>
+        &nbsp;designed to showcase examples of my work and the different&nbsp;
+        <Link href="/my-skills">
+          <a className="text-tertiary underline">skills</a>
+        </Link>
+        &nbsp;I have acquired along the way.
+      </p>
+    </div>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { Intro } from '../components/Index/Intro/Intro';
 
 const Home: NextPage = () => {
   return (
@@ -7,7 +8,7 @@ const Home: NextPage = () => {
       <Head>
         <title>Creative Developments | Home | Freelance Web Development</title>
       </Head>
-      <h1>Home</h1>
+      <Intro />
     </>
   );
 };
