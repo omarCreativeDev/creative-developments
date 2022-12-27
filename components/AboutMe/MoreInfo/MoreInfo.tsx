0e8e60deaeb5c5880e8164efda751e5bc8085855
Omar Mirza <omar.creative.dev@gmail.com>
2022-12-27 21:27:38 +0000

    added more info component

---
+++ components/AboutMe/MoreInfo/MoreInfo.tsx
@@ -0,0 +1,47 @@
+import Link from 'next/link';
+import { YearsOfExperience } from '../../Shared/YearsOfExperience/YearsOfExperience';
+
+export const MoreInfo = () => {
+  return (
+    <>
+      <h1>
+        About <span className="text-tertiary">me</span>
+      </h1>
+      <p>
+        Creative Developments aka Omar Mirza is a freelance front end web developer with
+        <YearsOfExperience /> years commercial experience located in Barkingside, London. My aim is
+        to develop{' '}
+        <Link href="/portfolio">
+          <a className="text-tertiary underline">amazing websites</a>
+        </Link>{' '}
+        that my clients and I both love.
+      </p>
+
+      <h2>Overview</h2>
+      <p>
+        I am proficient in taking complex{' '}
+        <Link href="/portfolio">
+          <a className="text-tertiary underline">projects</a>
+        </Link>{' '}
+        from conception to reality using solid programming principles and agile methodologies. I
+        possess a forward thinking approach to developing front end architecture that’s both
+        scalable and modular whilst also conforming to web accessibility standards.
+      </p>
+
+      <h3>
+        WHAT <span className="text-tertiary">I</span> DO
+      </h3>
+      <p>
+        Javascript, CSS3 and HTML5 and much more! See{' '}
+        <Link href="/my-skills">
+          <a className="text-tertiary underline">my skills</a>
+        </Link>{' '}
+        for full details. If you are interested in hiring me please{' '}
+        <Link href="/contact-me">
+          <a className="text-tertiary underline">get in touch</a>
+        </Link>
+        !
+      </p>
+    </>
+  );
+};
--- pages/about-me.tsx
+++ pages/about-me.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { MoreInfo } from '../components/AboutMe/MoreInfo/MoreInfo';
 
 const AboutMe: NextPage = () => {
   return (
@@ -7,7 +8,7 @@ const AboutMe: NextPage = () => {
       <Head>
         <title>Creative Developments | About Me | Freelance Web Development</title>
       </Head>
-      <h1>About me</h1>
+      <MoreInfo />
     </>
   );
 };
